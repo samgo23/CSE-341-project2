78203464a3201c13e255b32c5fcf61f582b0017a
samgo23 <gordsam18@gmail.com>
2023-05-13 22:22:47 -0600

    remove helmet

--- apps.js
+++ apps.js
@@ -5,7 +5,7 @@ const passport = require('passport');
 require('./config/passport');
 const session = require('express-session');
 const cors = require('cors');
-const helmet = require('helmet');
+
 
 const port = process.env.PORT || 8080;
 const app = express();
@@ -22,7 +22,14 @@ app
   .use(passport.initialize())
   .use(passport.session())
   .use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
+    const allowedOrigins = [
+      'http://localhost:8080',
+      'https://cse-341-project2-9tcn.onrender.com/'
+    ];
+    const origin = req.headers.origin;
+    if (allowedOrigins.includes(origin)) {
+      res.setHeader('Access-Control-Allow-Origin', origin);
+    }
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
     res.setHeader(
       'Access-Control-Allow-Headers',
@@ -31,21 +38,7 @@ app
     next();
   })
   .use(cors({ methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], origin: '*' }))
-  .use('/', require('./routes'))
-  .use(
-    helmet.contentSecurityPolicy({
-      directives: {
-        defaultSrc: ["'self'", 'https://cse-341-project2-9tcn.onrender.com'], // Allow content from the same origin
-        scriptSrc: ["'self'", "'unsafe-inline'", "'unsafe-eval'"], // Allow inline scripts and eval()
-        styleSrc: ["'self'", "'unsafe-inline'"], // Allow inline styles
-        imgSrc: ["'self'", "data:"], // Allow images from the same origin and data URIs
-        connectSrc: ["'self'"], // Allow connections only to the same origin
-        fontSrc: ["'self'"], // Allow fonts from the same origin
-        objectSrc: ["'none'"], // Disallow plugins like Flash and Silverlight
-        upgradeInsecureRequests: [], // Redirect HTTP requests to HTTPS
-      },
-    })
-  );
+  .use('/', require('./routes'));
   
 
 mongodb.initDb((err) => {
