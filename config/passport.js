e27edb9679e6f428b7aab4d5e6c9108550a91611
samgo23 <gordsam18@gmail.com>
2023-05-13 20:08:19 -0600

    GitHub error

--- config/passport.js
+++ config/passport.js
@@ -1,7 +1,7 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20');
 const User = require('../models/user');
-const GithubStrategy = require('passport-github2');
+const GitHubStrategy = require('passport-github2');
 
 passport.use(new GoogleStrategy({
   clientID: process.env.GOOGLE_CLIENT_ID,
