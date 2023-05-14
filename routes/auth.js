76ea0ce2f822929008f9b55a7fd35a213cb856d6
samgo23 <gordsam18@gmail.com>
2023-05-13 20:10:56 -0600

    router error

--- routes/auth.js
+++ routes/auth.js
@@ -16,20 +16,20 @@ router.get('/logout', function (req, res, next) {
   });
 });
 
-app.get('/auth/google',
+router.get('/auth/google',
   passport.authenticate('google', { scope: ['profile'] }));
 
-app.get('/auth/google/callback', 
+router.get('/auth/google/callback', 
   passport.authenticate('google', { failureRedirect: '/api-docs' }),
   function(req, res) {
     // Successful authentication, redirect home.
     res.redirect('/api-docs');
   });
 
-app.get('/auth/github',
+router.get('/auth/github',
   passport.authenticate('github', { scope: [ 'user:email' ] }));
 
-app.get('/auth/github/callback', 
+router.get('/auth/github/callback', 
   passport.authenticate('github', { failureRedirect: '/login' }),
   function(req, res) {
     // Successful authentication, redirect home.
