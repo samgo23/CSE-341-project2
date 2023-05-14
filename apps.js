bcf587e5365dcde0cd8474f43016fd657f301980
samgo23 <gordsam18@gmail.com>
2023-05-13 22:08:09 -0600

    helmet

--- apps.js
+++ apps.js
@@ -5,6 +5,7 @@ const passport = require('passport');
 require('./config/passport');
 const session = require('express-session');
 const cors = require('cors');
+const helmet = require('helmet');
 
 const port = process.env.PORT || 8080;
 const app = express();
@@ -30,7 +31,21 @@ app
     next();
   })
   .use(cors({ methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], origin: '*' }))
-  .use('/', require('./routes'));
+  .use('/', require('./routes'))
+  .use(
+    helmet.contentSecurityPolicy({
+      directives: {
+        defaultSrc: ["'self'"], // Allow content from the same origin
+        scriptSrc: ["'self'", "'unsafe-inline'", "'unsafe-eval'"], // Allow inline scripts and eval()
+        styleSrc: ["'self'", "'unsafe-inline'"], // Allow inline styles
+        imgSrc: ["'self'", "data:"], // Allow images from the same origin and data URIs
+        connectSrc: ["'self'"], // Allow connections only to the same origin
+        fontSrc: ["'self'"], // Allow fonts from the same origin
+        objectSrc: ["'none'"], // Disallow plugins like Flash and Silverlight
+        upgradeInsecureRequests: [], // Redirect HTTP requests to HTTPS
+      },
+    })
+  );
 
 mongodb.initDb((err) => {
   if (err) {
